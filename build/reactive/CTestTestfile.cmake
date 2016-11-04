4a06db85cf2f3770c347896cbaae65d5c76bd610
lycantropus <mechanical.maggot.0@gmail.com>
2016-11-04 13:16:20 +0000

    cleaned git

--- build/reactive/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lycantropus/catkin_ws/src/reactive
-# Build directory: /home/lycantropus/catkin_ws/build/reactive
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
